f53f4763c33c7acbb4261ac0dc5d7a0a21d9e659
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-28 02:23:20 +0300

    Added myReduce func

--- index.js
+++ index.js
@@ -21,3 +21,13 @@ function myMap(collection, callback){
     });
     return j;
 }
+
+// Added myReduce func 
+function myReduce(collection, callback, acc){
+    if(acc==undefined) {let acc=collection[0]}
+    let c = checks(collection)
+    c.forEach(e => {
+       acc = callback(acc, e, collection)
+    });
+    return acc;
+}
